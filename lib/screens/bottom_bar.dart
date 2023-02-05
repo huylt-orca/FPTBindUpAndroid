f006cad29ec18de7dbbdc42b60a5209985aa8374
Thanh Huy <88583984+huylt-orca@users.noreply.github.com>
2023-02-05 12:04:42 +0700

    project base

---
+++ lib/screens/bottom_bar.dart
@@ -0,0 +1,69 @@
+import 'package:android/screens/home_screen.dart';
+import 'package:android/screens/my_product_screen.dart';
+import 'package:android/screens/notification_screen.dart';
+import 'package:android/screens/profile_screen.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatefulWidget {
+  const BottomBar({Key? key}) : super(key: key);
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  int _selectedIndex = 0;
+  static final List<Widget> _widgetOptions = <Widget>[
+    HomeScreen(),
+    MyProductScreen(),
+   NotificationScreen(),
+    ProfileScreen(),
+  ];
+
+  void _onItemTapped(int index){
+    setState((){
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child:
+          Text("FPT ProductHunt"),
+        ),
+      ),
+      body: Center(
+        child: _widgetOptions[_selectedIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _onItemTapped,
+        currentIndex: _selectedIndex,
+        elevation: 10,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.blueGrey,
+        type: BottomNavigationBarType.fixed,
+        unselectedItemColor: const Color(0xFF526480),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
+              label: "Home"),
+          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_bookmark_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_bookmark_filled),
+              label: "Product"),
+          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_alert_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_alert_filled),
+              label: "Notification"),
+          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
+              label: "Profile"),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/my_product_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class MyProductScreen extends StatelessWidget {
+  const MyProductScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("My Product"),
+    );
+  }
+}
